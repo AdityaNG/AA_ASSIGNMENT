5c79008a5dd1037801b3cb9ad20ccf90aa9f8860
AdityaNG <adityang5@gmail.com>
2021-11-21 21:59:01 +0530

    Report creation

--- main.py
+++ main.py
@@ -1,92 +1,93 @@
+import time
 import numpy as np
 import binascii
 from scipy.fft import fft as sc_fft
-from implementations import dft, fft, ifft, ifft2
+from implementations import dft, fft, ifft, ifft2, chooseKeys, encrypt, decrypt
 
 np.set_printoptions(precision=2, suppress=True)  # for compact output
 
-print("\033[96mINFO\033[0m IFFT")
+def main(n):
 
-A = np.random.random(4)
-B = np.random.random(4)
+    print("\033[96mINFO\033[0m IFFT")
 
-print("\033[96mINFO\033[0m A =", A)
-print("\033[96mINFO\033[0m B =", B)
+    A = np.random.random(n)
+    B = np.random.random(n)
 
-A_dft = dft(A)
-B_dft = dft(B)
+    print("\033[96mINFO\033[0m A =", A)
+    print("\033[96mINFO\033[0m B =", B)
 
-print("\033[96mINFO\033[0m A_dft =", A_dft)
-print("\033[96mINFO\033[0m B_dft =", B_dft)
+    A_dft = dft(A)
+    B_dft = dft(B)
 
-if (np.allclose(A_dft, np.fft.fft(A)) and np.allclose(B_dft, np.fft.fft(B))):
-    print("\033[92mPASSED\033[0m DFT")
-else:
-    print("\033[91mFAILED\033[0m DFT")
+    print("\033[96mINFO\033[0m A_dft =", A_dft)
+    print("\033[96mINFO\033[0m B_dft =", B_dft)
 
-A_fft = fft(A)
-B_fft = fft(B)
+    if (np.allclose(A_dft, np.fft.fft(A)) and np.allclose(B_dft, np.fft.fft(B))):
+        print("\033[92mPASSED\033[0m DFT")
+    else:
+        print("\033[91mFAILED\033[0m DFT")
 
-print("\033[96mINFO\033[0m A_fft =", A_fft)
-print("\033[96mINFO\033[0m B_fft =", B_fft)
+    A_fft = fft(A)
+    B_fft = fft(B)
 
-
-if (np.allclose(A_fft, np.fft.fft(A)) and np.allclose(B_fft, np.fft.fft(B))):
-    print("\033[92mPASSED\033[0m FFT")
-else:
-    print("\033[91mFAILED\033[0m FFT")
-
-C = np.multiply(A_fft, B_fft)
-
-print("\033[96mINFO\033[0m C = pointwise_multiply( A_fft, B_fft )")
-print("\033[96mINFO\033[0m C =", C)
-
-import rsa
- 
-# generate public and private keys with
-# rsa.newkeys method,this method accepts
-# key length as its parameter
-# key length should be atleast 16
-publicKey, privateKey = rsa.newkeys(2**10)
-
-# this is the string that we will be encrypting
-message = C
-
-# rsa.encrypt method is used to encrypt
-# string with public key string should be
-# encode to byte string before encryption
-# with encode method
-C_encrypted = rsa.encrypt(message.tobytes(),
-                        publicKey)
-
-print("\033[96mINFO\033[0m C_encrypted =", binascii.hexlify(C_encrypted))
-
-# the encrypted message can be decrypted
-# with ras.decrypt method and private key
-# decrypt method returns encoded byte string,
-# use decode method to convert it to string
-# public key cannot be used for decryption
-C_decrypted = np.frombuffer(rsa.decrypt(C_encrypted, privateKey), dtype=np.complex128)
-
-print("\033[96mINFO\033[0m C_decrypted =", C_decrypted)
-
-if np.allclose(C, C_decrypted):
-    print("\033[92mPASSED\033[0m RSA Encyption + Decryption")
-else:
-    print("\033[91mFAILED\033[0m RSA Encyption + Decryption")
+    print("\033[96mINFO\033[0m A_fft =", A_fft)
+    print("\033[96mINFO\033[0m B_fft =", B_fft)
 
 
-# TODO : Inverse FFT
-A_ifft = np.zeros_like(A)
-B_ifft = np.zeros_like(B)
+    if (np.allclose(A_fft, np.fft.fft(A)) and np.allclose(B_fft, np.fft.fft(B))):
+        print("\033[92mPASSED\033[0m FFT")
+    else:
+        print("\033[91mFAILED\033[0m FFT")
 
-A_ifft = ifft(A_fft)
-B_ifft = ifft(B_fft)
+    C = np.multiply(A_fft, B_fft)
 
-print(A)
-print(A_ifft)
+    print("\033[96mINFO\033[0m C = pointwise_multiply( A_fft, B_fft )")
+    print("\033[96mINFO\033[0m C =", C)
 
-if np.allclose(A, A_ifft) and np.allclose(B, B_ifft):
-    print("\033[92mPASSED\033[0m IFFT")
-else:
-    print("\033[91mFAILED\033[0m IFFT")
+    message = "Secret Message" # C
+
+    print("Message to be encrypted = ", message)
+    print('Encrypting...')
+    C_encrypted = encrypt(message)
+    print(C_encrypted)
+    
+    #print("\033[96mINFO\033[0m C_encrypted =", binascii.hexlify(C_encrypted))
+    print("\033[96mINFO\033[0m C_encrypted =", C_encrypted)
+    
+    print('Decryption...')
+    C_decrypted = decrypt(C_encrypted)
+    print(C_decrypted)
+
+    if np.allclose(C, C_decrypted):
+        print("\033[92mPASSED\033[0m RSA Encyption + Decryption")
+    else:
+        print("\033[91mFAILED\033[0m RSA Encyption + Decryption")
+
+
+    # TODO : Inverse FFT
+    A_ifft = np.zeros_like(A)
+    B_ifft = np.zeros_like(B)
+
+    A_ifft = ifft(A_fft)
+    B_ifft = ifft(B_fft)
+
+    print(A)
+    print(A_ifft)
+
+    if np.allclose(A, A_ifft) and np.allclose(B, B_ifft):
+        print("\033[92mPASSED\033[0m IFFT")
+    else:
+        print("\033[91mFAILED\033[0m IFFT")
+
+if __name__=='__main__':
+    timings = {'x':[], 'y':[]}
+    for n in [2**i for i in range(2, 12)]:
+        start = time.time()
+        print("n=", n)
+        main(n)
+        print()
+        print("*"*10)
+        end = time.time()
+        timings['x'].append(n)
+        timings['y'].append(end-start)    
+    print(timings)
